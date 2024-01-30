56aa1ee8fa936451aa507def092c35241189bca8
CodeBear16 <40863036+CodeBear16@users.noreply.github.com>
2024-01-30 22:29:33 +0900

    Title: 정수 선언하고 뺄셈, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240130/정수 선언하고 뺄셈/define-numbers-and-substract.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int t_NumA = 97;
+    int t_NumB = 13;
+    int t_Sum = t_NumA - t_NumB;
+    std::cout << t_NumA << " - " << t_NumB << " = " << t_Sum << std::endl;
+    return 0;
+}
